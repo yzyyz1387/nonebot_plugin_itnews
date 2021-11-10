2b0a793e13d4c844fb7c33a1c325c55961230646
yzyyz1387 <1796031384@qq.com>
2021-11-10 21:25:59 +0800

    改为PyPI项目

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/10 21:12
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : __init__.py
+# @Software: PyCharm
---
+++ setup.py
@@ -0,0 +1,26 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/10 21:13
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : setup.py.py
+# @Software: PyCharm
+from setuptools import setup, find_packages
+
+setup(
+    name="nonebot_plugin_itnews",
+    version="0.1.0",
+    keywords=("pip", "nonebot2", "nonebot", "itnews", "nonebot_plugin"),
+    description="a nonebot plugin",
+    long_description="a nonebot plugin, generate pictures from news",
+    license="MIT Licence",
+
+    url="https://github.com/yzyyz1387/nonebot_plugin_itnews",
+    author="yzyyz1387",
+    author_email="youzyyz1384@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
